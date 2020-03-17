dd8afccf11f8f41b5c9712150ecf6fcd959ed15f
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-03-17 11:59:33 +0100

    Message changed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
-  console.log('DB connected succesfully');
+  console.log('DB connected succesfully...');
 });
 
 const port = process.env.PORT || 3000;
